afe2c19546d7282726aad94c62867a71d512ae38
jgsch <jgschmidtbr@gmail.com>
2025-11-15 20:00:42 -0300

    Teste WebSockets: conexao

---
+++ src/webSockets.test.tsx
@@ -0,0 +1,28 @@
+global.WebSocket = require("ws");
+
+import {
+  connectWebSocket,
+  isConnected,
+  getConnectionId,
+} from "./webSockets";
+
+jest.setTimeout(10000);
+
+afterAll(() => {
+  if ((global as any).socket && (global as any).socket.close) {
+    console.log("🔌 Fechando WebSocket...");
+    (global as any).socket.close();
+  }
+});
+
+describe("WebSocket AWS – Teste real", () => {
+  test("Conecta ao WebSocket e recebe connectionId", async () => {
+    const id = await connectWebSocket();
+
+    expect(id).toBeDefined();
+    expect(typeof id).toBe("string");
+    expect(isConnected()).toBe(true);
+
+    console.log("ConnectionId recebido:", id);
+  });
+});
